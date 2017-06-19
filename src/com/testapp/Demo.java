bb7ba51b6886e32bff67543ffa258fbaccae7fae
soumya <soumya@192.168.0.17>
2017-06-19 10:31:52 +0530

    #process1 has done

---
+++ src/com/testapp/Demo.java
@@ -0,0 +1,26 @@
+package com.testapp;
+
+public class Demo {
+	private int eid;
+	private String ename;
+	
+	
+	public Demo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+
+}
